5c9d39ba5d91fae403b7ba2d2ecae6e7a5ba33fb
KarLOL82 <klinfeldt@gmail.com>
2022-11-03 21:23:38 -0700

    added missing depency

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -53,14 +53,14 @@ const SavedBooks = () => {
 
       const updatedUser = await response.json();
       setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
+      
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // if data isn't here yet, say so
+  
   if (!userDataLength) {
     return <h2>LOADING...</h2>;
   }
