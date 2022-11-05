5dfb5229f2314eee0bc1b3857988f5911f0920fb
KarLOL82 <klinfeldt@gmail.com>
2022-11-04 19:54:05 -0700

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -25,6 +25,10 @@ if (process.env.NODE_ENV === 'production') {
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
   
   db.once('open', () => {
     app.listen(PORT, () => {
